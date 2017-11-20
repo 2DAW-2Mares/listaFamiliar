7f8c7670752eb83c836e5f8e30fcf7b08fb2d080
Alberto Sierra <alberto.sierra@murciaeduca.es>
2017-11-20 11:18:53 +0100

    configurando el entorno

---
+++ server/datasources.local.js
@@ -0,0 +1,16 @@
+// Copyright IBM Corp. 2014. All Rights Reserved.
+// Node module: loopback-example-offline-sync
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+var config = require('./config.local.js')
+
+module.exports = {
+  mysqlDs: {
+    connector: 'mysql',
+    hostname: config.db_host,
+    port: config.db_port,
+    user: config.db_user,
+    password: config.db_password,
+    database: 'lista_familiar',
+  }
+};
