c316f2558607613e8cb52e2371c38ebeb3a618c9
Alberto Sierra <alberto.sierra@murciaeduca.es>
2017-11-20 12:54:46 +0100

    Asignando como 'owner' al usuario que ha creado la lista familiar

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -2,4 +2,10 @@
 
 module.exports = function(Listafamiliar) {
 
+	// Asignar como 'owner' de la lista que se va a crear, al usuario que solicita su creación
+    Listafamiliar.beforeRemote('create', function (context, listaFamiliar, next) {
+        context.args.data.owner = context.req.accessToken.userId;
+        next();
+    });
+  
 };
