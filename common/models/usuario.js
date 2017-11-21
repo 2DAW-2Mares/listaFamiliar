a85535e646219771ba8f970df785001bcf2ea9d3
Alberto Sierra <alberto.sierra@murciaeduca.es>
2017-11-21 22:36:00 +0100

    Cualquier usuario de una lista puede aceptar solicitudes llegadas a dicha lista

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -2,4 +2,60 @@
 
 module.exports = function(Usuario) {
 
+	/**
+	 * Le enviamos un identificador de usuario y, si ese usuario tiene alguna solicitud en la lista de la que es miembro el actualmente autenticado, esta solicitud será aprobada.
+	 * @param {object} request La petición que ha dado lugar a la ejecución de este método
+	 * @param {Function(Error, array)} callback
+	 */
+
+	Usuario.prototype.aceptarSolicitud = function(request, callback) {
+		var usuariosEnLista = [];
+
+		// Como es un método de instancia, podemos utilizar this. En este caso, guardamos this como usuarioSolicitante.
+		var usuarioSolicitante = this;
+
+		// Guardamos el id del usuario autenticado
+		var autenticadoId = request.accessToken.userId;
+
+		//¿Cuál es la listaFamiliar asociada al usuario autenticado
+		Usuario.findById(autenticadoId, function(err, usuarioAutenticado) {
+			if (err) callback(err);
+
+			Usuario.find({
+					where: {
+						listaFamiliarId: usuarioAutenticado.listaFamiliarId
+					}
+				},
+				function(err, usuariosEnLista) {
+					if (err) callback(err);
+					// ¿El solicitante ha realizado alguna solicitud a la lista del usuarioAutenticado?
+					usuarioSolicitante.solicitudes.findById(usuarioAutenticado.listaFamiliarId, function(err, listaFamiliar) {
+						// Si no se encuentra la relación entre el usuario solicitante y la listafamiliar devuelve un error 
+						if (err) callback(err);
+
+						// asociar la listaFamiliar del autenticado al solicitante
+						usuarioSolicitante.listaFamiliarId = usuarioAutenticado.listaFamiliarId;
+
+						// Guardamos los cambios del usuarioSolicitante
+						usuarioSolicitante.save(function(err, usuarioSolicitante) {
+							
+							// Añadimos al usuario al array de usuarios de la listaFamiliar para devolverlo posteriormente
+							usuariosEnLista.push(usuarioSolicitante);
+
+							if (err) callback(err);
+
+							//borramos la solicitud
+							usuarioSolicitante.solicitudes.remove(listaFamiliar, function(err) {
+								if (err) callback(err);
+								callback(null, usuariosEnLista);
+							})
+						})
+					})
+
+				}
+			);
+		})
+	};
+
+
 };
